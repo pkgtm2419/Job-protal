b5e63d94c52fdabb719f96e993c5f2fd61583265
pkgtm2419 <pawangtm2419@gmail.com>
2023-02-07 18:31:52 +0530

    AboutUs, ContactUs and user Profile Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { AppGuard } from './app.guard';
 
 const routes: Routes = [
   { path: 'dashboard', loadChildren: () => import('./dashborad/dashborad.module').then(m => m.DashboradModule), canActivate: [AppGuard] },
+  { path: 'user-profile', loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule), canActivate: [AppGuard] },
   { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'about-us', loadChildren: () => import('./_public/about-us/about-us.module').then(m => m.AboutUsModule) },
+  { path: 'contact-us', loadChildren: () => import('./_public/contact-us/contact-us.module').then(m => m.ContactUsModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
